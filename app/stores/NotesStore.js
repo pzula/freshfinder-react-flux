4856e2e531e84891f8e38dccf5b38f223ed08071
Persa Zula <persa@persazula.com>
2015-03-13 17:08:24 -0400

    Adds MarketStore and constants

---
+++ app/stores/NotesStore.js
@@ -0,0 +1,52 @@
+var AppDispatcher = require('../dispatcher/AppDispatcher');
+var appConstants = require('../constants/appConstants');
+var EventEmitter = require('events').EventEmitter;
+var objectAssign = require('react/lib/Object.assign');
+
+var CHANGE_EVENT = 'change';
+
+var _state = {
+  notes = []
+  market: '',
+};
+
+var addNote = function(note){
+  _state.notes.push(note);
+}
+
+var changeMarket = function(newMarketObj){
+  _state = {
+    market: newMarketObj.market;
+    notes: newMarketObj.notes;
+  };
+};
+
+var notesStore = objectAssign({}, EventEmitter.prototype, {
+  getState: function(){
+    return _state;
+  },
+  addChangeListener: function(cb){
+    this.on(CHANGE_EVENT, cb);
+  },
+  removeChangeListener: function(cb){
+    this.removeChangeListener(CHANGE_EVENT, cb);
+  },
+});
+
+AppDispatcher.register(function(payload){
+  var action = payload.action;
+  switch(action.actionType){
+    case appConstants.ADD_NOTE:
+      addNote(action.data);
+      noteStore.emit(CHANGE_EVENT);
+      break;
+    case appConstants.CHANGE_MARKET:
+      changeMarket(action.data);
+      noteStore.emit(CHANGE_EVENT);
+      break;
+    default:
+      return true;
+  }
+});
+
+module.exports = noteStore;
