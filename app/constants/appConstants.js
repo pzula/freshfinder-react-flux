3bb92af68960bac78900658dcc9e4938932433c5
Persa Zula <persa@persazula.com>
2015-03-13 20:24:39 -0400

    Adds FirebaseUtils for communicating with Firebase API

--- app/constants/appConstants.js
+++ app/constants/appConstants.js
@@ -2,7 +2,8 @@ var appConstants = {
   FRESHFINDER_MARKET_DATA: "FRESHFINDER_MARKET_DATA"
   FRESHFINDER_CHANGE_MARKET: "FRESHFINDER_CHANGE_MARKET",
   ADD_NOTE: "ADD_NOTE",
-  CHANGE_MARKET: "CHANGE_MARKET"
+  CHANGE_MARKET: "CHANGE_MARKET",
+  FIREBASE_HOST: 'https://freshfinder-flux.firebaseio.com/notes'
 };
 
 module.exports = appConstants;
